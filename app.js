5939fd6c918f88ed1b0068ee766059485df6e478
lucaseduardopedroso <lucaseduardosilvapedroso@gmail.com>
2022-02-03 11:04:02 -0300

    Add data from webpage to the server

--- app.js
+++ app.js
@@ -1,31 +1,29 @@
 const express = require("express");
 const app = express();
-
+var items = ["Buy food", "Cook a dish", "Have a meal"];
 app.use(express.urlencoded({ extended: true }))
 app.set('view engine', 'ejs');
 
 app.get("/", function(req, res){
     var today = new Date();
-    var currentDay = today.getDay();
-    var day = "";
+    
+    var options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    };
 
-    if (currentDay === 1){
-        day = "Monday";
-    } else if (currentDay === 2){
-        day = "Tuesday";
-    } else if (currentDay === 3){
-        day = "Wednesday";
-    } else if (currentDay === 4){
-        day = "Thursday";
-    } else if (currentDay === 5){
-        day = "Friday";
-    } else if (currentDay === 6){
-        day = "Saturday";
-    } else {
-        day = "Sunday";
-    }
+    var day = today.toLocaleDateString("en-US", options);
 
-    res.render("list", {kindofDay: day});
+    res.render("list", {kindofDay: day, newListItem: items});
+});
+
+app.post("/", function(req, res){
+    var item = req.body.newItem;
+    
+    items.push(item);
+
+    res.redirect("/");
 });
 
 app.listen(3000, function(){
